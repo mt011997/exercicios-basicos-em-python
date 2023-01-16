fe58040a2fe64bd10819c90cd612fd1512de9178
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-01-16 01:29:59 -0300

    feat: Resolution of the first 40 exercises out of 53

--- exercicios.py
+++ exercicios.py
@@ -1,102 +1,221 @@
 # 1- Faça um programa que leia  um número inteiro e o imprima.
 numExe1 = 10
-print(f'Número inteiro: {numExe1}')
+print(f'Exercicio 01 - {numExe1}')
 
 # 2- Faça um programa que leia um número real e o imprima.
 numRealExe2 = 10.50
-print(f"Número real: {numRealExe2}")
+print(f"Exercicio 02 - {numRealExe2}")
 
 # 3- Peça ao usuário  para digitar 3 valores inteiros e imprima a soma deles.
-num1Exe3 = int(input('Digite um número inteiro '))
-num2Exe3 = int(input('Digite outro número inteiro '))
-num3Exe3 = int(input('Digite mais um número inteiro '))
-resultadoExe3 = num1Exe3 + num2Exe3 + num3Exe3
-print(f'A soma total dos números digitados é de: {resultadoExe3}')
+# num1Exe3 = int(input('Digite um número inteiro '))
+# num2Exe3 = int(input('Digite outro número inteiro '))
+# num3Exe3 = int(input('Digite mais um número inteiro '))
+# resultadoExe3 = num1Exe3 + num2Exe3 + num3Exe3
+# print(f'Exercicio 03 - {resultadoExe3}')
 
 # 4- Leia um número real e imprima o quadrado desse número.
 numRealExe4 = 8.50
 resultadoExe4 = numRealExe4 ** 2
-print(f'8.50 elevado a 2 é: {resultadoExe4}')
+print(f'Exercicio 04 - {resultadoExe4}')
 
 # 5 - Leia um número real e imprima a quinta parte desse número.
 numRealExe5 = 37.83
 resultadoExe5 = numRealExe5 / 5
-print(f'A quinta parte de 37.83 é de: {resultadoExe5}')
+print(f'Exercicio 05 - {resultadoExe5}')
 
 # 6- Leia uma temperatura em Celsius e converta para Fahrenheit.
 tempExe6 =  20.0
 fahrenheitExe6 = tempExe6 * (9.0 / 5.0) + 32.0
-print(f'{tempExe6}°C em fahrenheit, fica: {fahrenheitExe6}°F')
+print(f'Exercicio 06 - {fahrenheitExe6}°F')
 
 # 7- Leia uma temperatura em Fahrenheit e converta para Celsius.
 tempExe7 = 68.0
 celsiusExe7 = 5.0 * (tempExe7 - 32.0) / 9.0
-print(f'{tempExe7}°F em celsius, fica: {celsiusExe7}°C')
+print(f'Exercicio 07 - {celsiusExe7}°C')
 
 # 8- Leia uma temperatura em Kelvin e converta para Celsius.
 tempExe8 = 293.15
 celsiusExe8 = tempExe8 - 273.15
-print(f'{tempExe8}°K em celsius, fica: {celsiusExe8}°C')
+print(f'Exercicio 08 - {celsiusExe8}°C')
 
 # 9- Leia uma temperatura em Celsius e converta para Kelvin
 tempExe9 = 20.0
 kelvinExe9 = tempExe9 + 273.15
-print(f'{tempExe9}°C em kelvin, fica: {kelvinExe9}°K')
+print(f'Exercicio 09 - {kelvinExe9}°K')
 
 # 10- Leia uma velocidade em Km/h e converta em metros por segundo
 kmExe10 = 60
 metrosExe10 = kmExe10 / 3.6
-print(f'{kmExe10}km/h em metros por segundo, é: {metrosExe10}m/s')
+print(f'Exercicio 10 - {metrosExe10}m/s')
 
 # 11- Leia uma velocidade em metros por segundo e converta em km/h
 metrosExe11 = 17
 kmExe11 = metrosExe11 * 3.6
-print(f'{metrosExe11}m/s em km/h, é: {kmExe11}km/h')
+print(f'Exercicio 11 - {kmExe11}km/h')
 
 # 12- Leia a distância em milhas e converta em quilometros
 milhasExe12 = 100
 quilometrosExe12 = 1.61 * milhasExe12
-print(f'{milhasExe12} milhas convertida para quilometros, fica: {quilometrosExe12} quilometros')
+print(f'Exercicio 12 - {quilometrosExe12} quilometros')
 
 # 13- Leia a distancia em quilometros e converta para milhas
 quilometrosExe13 = 60
 milhasExe13 = quilometrosExe13 / 1.61
-print(f'{quilometrosExe13} quilometros convertido para milhas, fica: {milhasExe13} milhas')
+print(f'Exercicio 13 - {milhasExe13} milhas')
 
 # 14- Leia um Angulo em graus e converta para radianos
 grauExe14 = 30
 radianoExe14 = grauExe14 * 3.14 /180
-print(f'{grauExe14} graus de angulo para radiano, fica: {radianoExe14} radianos')
+print(f'Exercicio 14 - {radianoExe14} radianos')
 
 # 15- Leia um Angulo em radianos e converta para graus
 radianoExe15 = 5
 grausExe15 = radianoExe15 * 180 / 3.14
-print(f'{radianoExe15} radianos de angulo para graus, fica: {grausExe15} graus')
+print(f'Exercicio 15 - {grausExe15} graus')
 
 # 16- Leia um valor em polegadas e converta para centímetros
 polegadaExe16 = 40
 centimetroExe16 = polegadaExe16 * 2.54
-print(f'{polegadaExe16} polegadas em centimetros, é: {centimetroExe16} centímetros')
+print(f'Exercicio 16 - {centimetroExe16} centímetros')
 
 # 17- Leia um valor em centímetros e converta para polegadas
 centimetroExe17 = 90
 polegadaExe17 = centimetroExe17 / 2.54
-print(f'{centimetroExe17} centímetros em polegadas, é: {polegadaExe17} polegadas')
+print(f'Exercicio 17 - {polegadaExe17} polegadas')
 
 # 18- Leia um valor em metros cúbicos e converta para litros
 cubicoExe18 = 20
 litroExe18 = 1000 * cubicoExe18
-print(f'{cubicoExe18} m³ em litros, é: {litroExe18}')
+print(f'Exercicio 18 - {litroExe18}')
 
 # 19- Leia um valor em litros e converta para metros cubicos
 litroExe19 = 20
 cubicoExe19 = litroExe19 / 1000
-print(f'{litroExe19} litros em m³, é: {cubicoExe18}')
+print(f'Exercicio 19 - {cubicoExe18}')
 
-#20- Leia um valor em quilogramas e converta para libras
+# 20- Leia um valor em quilogramas e converta para libras
 quilogramaExe20 = 50
 librasExe20 = quilogramaExe20 / 0.45
-print(f'{quilogramaExe20} quilogramas convertido em libras, é: {librasExe20}')
+print(f'Exercicio 20 - {librasExe20}')
 
+# 21- Leia um valor em libras e converte para quilogramas
+librasExe21 = 20
+quilogramaExe21 = librasExe21 * 0.45
+print(f'Exercicio 21 - {quilogramaExe21}')
 
+# 22- Leia um valor em jardas e converta para metros
+jardaExe22 = 20
+metroExe22 = 0.91 * jardaExe22
+print(f'Exercicio 22 - {metroExe22}')
 
+# 23- Leia um valor em metros e converta para jardas
+metroExe23 = 20
+jardaExe23 = metroExe23 / 0.91
+print(f'Exercicio 23 - {jardaExe23}')
+
+# 24- Leia um valor em metros quadrados e converta em acres
+metrosQuadradosExe24 = 20
+acreExe24 = metrosQuadradosExe24 * 0.000247
+print(f'Exercicio 24 - {acreExe24}')
+
+# 25- Leia um valor em acres e converta para metros quadrados
+acreExe25 = 20
+metrosQuadradosExe25 = acreExe24 * 4048.58
+print(f'Exercicio 25 - {metrosQuadradosExe25}')
+
+# 26- Leia um valor em metros quadrados e converta para hectares
+metrosQuadradosExe26 = 20
+hectaresExe26 = metrosQuadradosExe26 * 0.0001
+print(f'Exercicio 26 - {hectaresExe26}')
+
+# 27- Leia um valor em hectares e converta para metros quadrados
+hectaresExe27 = 20
+metrosQuadradosExe27 = hectaresExe27 * 1000
+print(f'Exercicio 27 - {metrosQuadradosExe27}')
+
+# 28- Leia 3 valores e apresente como resultado a soma dos quadrados dos 3 valores
+valor1Exe28 = 87
+valor2Exe28 = 95
+valor3Exe28 = 62
+resultadoExe28 = valor1Exe28 ** 2 + valor2Exe28 ** 2 + valor3Exe28 ** 2
+print(f'Exercicio 28 - {resultadoExe28}')
+
+# 29- Leia um numero inteiro e impria a soma do sucessor de seu triplo com o antecessor de seu dobro
+valor1Exe29 = 20
+antecessorExe29 = valor1Exe29 - 1
+sucessorExe29 = valor1Exe29 + 1
+resultadoExe29 = antecessorExe29 * 2 + sucessorExe29 * 3
+print(f'Exercicio 29 - {resultadoExe29}')
+
+# 30- Leia o valor em real e converta para dolar
+realExe30 = 50.00
+valorDolarExe30 = realExe30 /  5.10
+print(f'Exercicio 30 - {valorDolarExe30}')
+
+# 31- Leia um numero inteiro e imprima seu antecessor e sucessor
+numeroExe31 = 60
+sucessorExe31 = numeroExe31 + 1
+antecessorExe31 = numeroExe31 -1
+print(f'Exercicio 31 - Antecessor {antecessorExe31}, Sucessor {sucessorExe31}')
+
+# 32- Leia 3 valores e apresente como resultado a soma dos quadrados dos 3 valores
+valor1Exe32 = 12
+valor2Exe32 = 18
+valor3Exe32 = 60
+resultadoExe32 = valor1Exe32 ** 2 + valor2Exe32 ** 2 + valor3Exe32 ** 2
+print(f'Exercicio 32 - {resultadoExe32}')
+
+# 33- Leia o tamanho de um lado do quadrado e imprima o resultado da area
+ladoQuadradoExe33 = 40
+areaQuadradoExe33 = ladoQuadradoExe33 ** 2
+print(f'Exercicio 33 - {areaQuadradoExe33}')
+
+# 34- Leia o valor do raio de um circulo e calcule a area
+raioCirculoExe34 = 30
+areaCirculoExe34 = 3.14 * raioCirculoExe34 ** 2
+print(f'Exercicio 34 - {areaCirculoExe34}')
+
+# 35- Calcule a hipotenusa de um triangulo
+catetoAExe35 = 20
+catetoBExe35 = 18
+resultadoExe35 = (catetoAExe35 ** 2 + catetoBExe35 ** 2) ** 0.5
+print(f'Exercicio 35 - {resultadoExe35}')
+
+# 36- Leia a altura e o raio de um cilintro circular e imprima o volume do cilindro
+raioCilintroExe36 = 20
+alturaCilintroExe36 = 60
+resultadoExe36 = 3.14 * raioCilintroExe36 ** 2 * alturaCilintroExe36
+print(f'Exercicio 36 - {resultadoExe36}')
+
+# 37- Leia um valor que tenha 12% de desconto
+valorExe37 = 80
+descontoExe37 = 12 / 100 * valorExe37
+resultadoExe37 = valorExe37 - descontoExe37
+print(f'Exercicio 37 - {resultadoExe37}')
+
+# 38- Leia um valor que recebeu aumento de 25%
+valorExe38 = 1273.00
+aumentoExe38 = 25/ 100 * valorExe38
+resultadoExe37 = valorExe38 + aumentoExe38
+print(f'Exercicio 38 - {resultadoExe37}')
+
+# 39- O valor de 780.000,00 sera dividido entre 3 ganhadores
+# O primeiro receberá 48%
+# O seguundo 32%
+# O terceiro o restante
+# imprima a quantinha ganha por cada ganhador
+
+valorExe39 = 780000.00
+ganhador1Exe39 = 48/100 * valorExe39
+ganhador2Exe39 = 32/100 * valorExe39
+ganhador3Exe39 = 20/100 * valorExe39
+print(f'Exercicio 39 - Ganhador 1 : {ganhador1Exe39}, Ganhador 2 : {ganhador2Exe39}, Ganhador 3 : {ganhador3Exe39}')
+
+# 40- Uma empresa contrata um encanador a R$30,00 por dia. Faça um programa que solicite o número de dias
+# trabalhados pelo encanador e imprima a quantia líquida que deverá ser paga, sabendo que são descontados
+# 8% para impostos
+diasExe40 = 60
+totalExe40 = diasExe40 * 30
+descontoExe40 = 8/100 * totalExe40
+resultadoExe40 = totalExe40 - descontoExe40
+print(f'Exercicio 40 - {resultadoExe40}')
